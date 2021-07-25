eed228769ae3d2b776ac6ccbd3843137703ddbe0
Aditya <adityathakur532@gmail.com>
2021-07-25 15:43:49 +0530

    pagination added

--- src/components/People.js
+++ src/components/People.js
@@ -1,21 +1,35 @@
 import React from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import Person from "./person";
-
+import MyPagination from "./PaginationComponent";
 //Create a client
 const queryClient = new QueryClient();
-
 //Fetch data from api
 const fetchPeople = async () => {
   const res = await fetch(`http://swapi.dev/api/people/`);
   return res.json();
 };
-
 //Queries
 const People = () => {
-  const { data, status } = useQuery(["people"], fetchPeople);
-  console.log(data);
+  const [personList, setTagList] = React.useState([]);
+  const [currPage, setCurrPage] = React.useState(3);
 
+  React.useEffect(() => {
+    afterPageClicked(3);
+  }, []);
+
+  const { data, status } = useQuery(["people"], fetchPeople);
+  // console.log(data);
+  const afterPageClicked = (page_number) => {
+    setCurrPage(page_number);
+    fetch(`https://dummyapi.io/data/api/tag?limit=10&page=${page_number}`, {
+      headers: {
+        "app-id": "60983c4f56c487f12fd13e23"
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => setTagList(data.data));
+  };
   return (
     <div>
       {/* <p>{status}</p> */}
@@ -28,7 +42,23 @@ const People = () => {
           ))}
         </div>
       )}
+
+      <MyPagination
+        totPages={20}
+        currentPage={currPage}
+        pageClicked={(ele) => {
+          afterPageClicked(ele);
+        }}
+      >
+        <ul>
+          {personList.map((ele, ind) => {
+            return <li key={ele + ind}>{ele}</li>;
+          })}
+        </ul>
+      </MyPagination>
+
     </div>
+
   );
 };
 
